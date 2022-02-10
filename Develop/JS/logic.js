9b8d5aa0844517dc6603d47a4fc41300867d28d9
ARod <ajr.rodriguez095@gmail.com>
2022-02-09 20:09:03 -0500

    games list fetch call

--- Develop/JS/logic.js
+++ Develop/JS/logic.js
@@ -10,3 +10,19 @@ element.classList.remove('animate');
 setTimeout(function () {
     element.classList.add('animate');
 }, 5000);
+
+
+// call api to get games
+var listOfGames = function() {
+    var rawgApiKey = "ad7969f5577647f8b8809a70ca6cecd4";
+    var rawgUrl = `https://api.rawg.io/api/games?key=${rawgApiKey}`;
+
+    //fetch(rawgUrl);
+
+
+// display 8 games randomly to choose from. 
+// ?genres= blah blah bla (userInput)
+// ?tags= single or multiplayer (userInput)
+}
+
+//listOfGames();
