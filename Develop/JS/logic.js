f5b828405720d9121220522da888bf9e29411486
Nicolo Danna <ndanna128@gmail.com>
2022-02-09 20:57:09 -0500

    added some psuedo code

--- Develop/JS/logic.js
+++ Develop/JS/logic.js
@@ -10,3 +10,13 @@ element.classList.remove('animate');
 setTimeout(function () {
     element.classList.add('animate');
 }, 5000);
+
+
+// ask user if they want to play a single player game or multiplayer game
+// list single player games if user chooses single player
+// list multiplayer game if user chooses multiplayer
+// randomly generate 8 games from use choice
+// add choices to wheel
+//spin wheel
+// when wheel lands on game. give info about game
+// if player wants to spin again repeat process
