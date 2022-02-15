f11785c36ec8efa7a889923adf16a20d900d7f9b
Cherboi <rplatzer98@gmail.com>
2022-02-15 12:45:30 -0500

    centered contanier

--- Develop/JS/logic.js
+++ Develop/JS/logic.js
@@ -4,72 +4,70 @@ var btnChoiceEl = document.querySelector(".btn-container");
 var btnSingleEl = document.querySelector(".btn-sngl");
 var btnMultiEl = document.querySelector(".btn-multi");
 var gameListEl = document.querySelector(".game-list");
-     
+
 
 
 // call api to get games
-var getListOfGames = function(event) {
+var getListOfGames = function (event) {
 
     const rawgApiKey = "ad7969f5577647f8b8809a70ca6cecd4";
 
-   // request singleplayer games
-    if(event.target.matches(".btn-sngl")) {
+    // request singleplayer games
+    if (event.target.matches(".btn-sngl")) {
 
         var rawgUrl = `https://api.rawg.io/api/games?key=${rawgApiKey}&tags=singleplayer`;
 
-    //URL Games request
-    fetch(rawgUrl).then(function(response) {
-       response.json().then(function(data) {
-           console.log(data);
+        //URL Games request
+        fetch(rawgUrl).then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
 
-           
 
-          // loop through data and return 8 titles from object 
-           for(var i = 0 ; i < 8; i++) {
-               console.log(data.results[i].name);
-               
-                var gamebtnEl = document.createElement("div");
-                var games = document.createElement("button");
-                games.textContent = data.results[i].name;
 
-                gamebtnEl.appendChild(games);
-                gameListEl.appendChild(gamebtnEl)
+                // loop through data and return 8 titles from object 
+                for (var i = 0; i < 8; i++) {
+                    console.log(data.results[i].name);
 
-           };
+                    var gamebtnEl = document.createElement("div");
+                    var games = document.createElement("button");
+                    games.textContent = data.results[i].name;
 
-           //returns first game title
-           //console.log(data.results[0].name);
-       });
-    });
+                    gamebtnEl.appendChild(games);
+                    gameListEl.appendChild(gamebtnEl)
+
+                };
+
+                //returns first game title
+                //console.log(data.results[0].name);
+            });
+        });
     }
     // request multiplayer games 
-   else if(event.target.matches(".btn-multi")) {
-     
-    var rawgUrl = `https://api.rawg.io/api/games?key=${rawgApiKey}&tags=multiplayer`;
+    else if (event.target.matches(".btn-multi")) {
 
-    // URL Games request
-    fetch(rawgUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
+        var rawgUrl = `https://api.rawg.io/api/games?key=${rawgApiKey}&tags=multiplayer`;
 
-            // loop through data and return 8 titles from object 
-           for(var i = 0; i < 8; i++) {
-            console.log(data.results[i].name);
+        // URL Games request
+        fetch(rawgUrl).then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
 
-            var gamebtnEl = document.createElement("div");
-            var games = document.createElement("button");
-            games.textContent = data.results[i].name;
+                // loop through data and return 8 titles from object 
+                for (var i = 0; i < 8; i++) {
+                    console.log(data.results[i].name);
 
-            gamebtnEl.appendChild(games);
-            gameListEl.appendChild(gamebtnEl)
-            
-            
-         };
+                    var gamebtnEl = document.createElement("div");
+                    var games = document.createElement("button");
+                    games.textContent = data.results[i].name;
+
+                    gamebtnEl.appendChild(games);
+                    gameListEl.appendChild(gamebtnEl)
+
+
+                };
+            });
         });
-     });
     };
-
-
 };
 
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # random-game-generator
+
+with this random game generator you will be able to choose from a random 8 games,
+divided in to 2 catagories;
+Single Player and Mulitplayer 
